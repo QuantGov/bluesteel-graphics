c3c9d81e81c2198ef7fabf8b7c3da8583d81b308
Michael Gasvoda <mgasvoda@mercatus.gmu.edu>
2017-12-12 11:19:10 -0500

    moving fonts and improving path

--- bluesteel/graphics/graphics.py
+++ bluesteel/graphics/graphics.py
@@ -17,8 +17,7 @@ import matplotlib.font_manager as font_manager
 from pathlib import Path
 from PIL import Image as image
 
-
-font_dirs = ['./fonts/']
+font_dirs = [str(Path(__file__).parent.joinpath('fonts'))]
 font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
 font_list = font_manager.createFontList(font_files)
 font_manager.fontManager.ttflist.extend(font_list)
