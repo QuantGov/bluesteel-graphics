54ba3b002bf84db4cb69b532498e4f2c53ea9429
Jonathan Nelson <jnelson@mercatus.gmu.edu>
2018-05-14 13:14:40 -0400

    circleci fix

--- bluesteel/graphics/specific_formatting.py
+++ bluesteel/graphics/specific_formatting.py
@@ -251,8 +251,8 @@ def axis_labels_hbar(fig, ax, data, xtick_loc=None, xticklabels=None,
         ax.set_xticklabels(xtick_labels)
     if not yticklabels:
         yticklabels = data.index.values
-        if (type(yticklabels[0]) != str
-                and type(yticklabels[0]) != datetime.datetime):
+        if (type(yticklabels[0]) != str and 
+                type(yticklabels[0]) != datetime.datetime):
             if max(yticklabels) >= 1000000:
                 if any([i % 1000 for i in yticklabels]):
                     yticklabels = ['' if not i else f"{i / 1000:,}" for i in
